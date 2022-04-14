e0df8c84f95dea2e1c7e4e7bd827da41419df9ca
rahmouni oussama <ousrah@hotmail.com>
2022-04-14 11:58:34 +0000

    ok

---
+++ test1/mailing.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="mailing.aspx.cs" Inherits="test1.mailing" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Sujet :
+            <asp:TextBox ID="TextBox1" runat="server" Width="528px"></asp:TextBox>
+            <br />
+            Déstinaitaire :
+            <asp:TextBox ID="TextBox2" runat="server" Width="527px"></asp:TextBox>
+            <br />
+            Message :<br />
+            <asp:TextBox ID="TextBox3" runat="server" Height="214px" TextMode="MultiLine" Width="650px"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="envoyer" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ test1/mailing.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net;
+
+namespace test1
+{
+    public partial class mailing : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("tdi2022@outlook.fr");
+            mail.Subject = TextBox1.Text;
+            mail.Body = "inscription à <b> ismo.ma</b><br /><br /><a href = 'https://www.ismo.ma' > Suivez le lien</ a > ";
+            mail.IsBodyHtml = true;
+         /*   foreach (var attachment in attachments)
+                mail.Attachments.Add(new Attachment(attachment));
+         */
+          
+            //foreach (var address in toAddresses)
+                mail.To.Add(TextBox2.Text);
+
+            SmtpClient smtp = new SmtpClient("smtp.live.com", 587); //smtp.gmail.com port(578 ou 25 ou 64)
+         
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("tdi2022@outlook.fr", "ISMO@2022");
+
+            try
+            {
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
