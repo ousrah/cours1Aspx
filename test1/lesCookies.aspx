569cb80c520865e3f3bf8709dbf7a80027fefc5b
rahmouni oussama <ousrah@hotmail.com>
2022-04-18 09:05:08 +0000

    ok

---
+++ test1/lesCookies.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="lesCookies.aspx.cs" Inherits="test1.lesCookies" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Ecrire cookie" />
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="lire cookie" />
+&nbsp;<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        <asp:Button ID="Button3" runat="server" Text="supprimer cookie" OnClick="Button3_Click" />
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+</asp:Content>
---
+++ test1/lesCookies.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace test1
+{
+    public partial class lesCookies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpCookie c = new HttpCookie("maCookie");
+            c["nom"]= TextBox1.Text; 
+            c["prenom"]=TextBox2.Text;
+            c.Expires = DateTime.Now.AddDays(7);
+            Response.Cookies.Add(c);
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            HttpCookie c = Request.Cookies["maCookie"];
+            if (c != null)
+                Label1.Text = c["nom"] + " " + c["prenom"];
+            else
+                Label1.Text = "";
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            HttpCookie c = Request.Cookies["maCookie"];
+            c.Expires= DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(c);
+
+        }
+    }
+}
